457b5e2e38a320152b490e10ac7b1975ed6c037c
Ivan Borzenkov <ivan1986@list.ru>
2012-04-19 16:47:00 +0400

    Проверка несуществующего ключа - тест

--- src/Ivan1986/DebBundle/Tests/Entity/GpgKeyTest.php
+++ src/Ivan1986/DebBundle/Tests/Entity/GpgKeyTest.php
@@ -5,6 +5,7 @@ namespace Ivan1986\DebBundle\Tests\Entity;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Ivan1986\DebBundle\Entity\GpgKeyRepository;
 use Ivan1986\DebBundle\Entity\GpgKey;
+use Ivan1986\DebBundle\Exception\GpgNotFound;
 
 class GpgKeyTest extends WebTestCase
 {
@@ -25,11 +26,22 @@ class GpgKeyTest extends WebTestCase
         $keys = $this->em->getRepository('Ivan1986DebBundle:GpgKey');
         /** @var $keys GpgKeyRepository */
         $id = '28FA7071';
-        $key = $keys->getFromServer('28FA7071', 'keyserver.ubuntu.com');
+        $key = $keys->getFromServer($id, 'keyserver.ubuntu.com');
         /** @var $key GpgKey */
         $this->assertEquals($key->getId(), $id);
         $this->assertEquals($key->getFingerprint(), '33C640DA31127882C496917F6831CF9528FA7071');
-        //$this->assertEquals($key->getData(), '33C640DA31127882C496917F6831CF9528FA7071');
+        $this->assertEquals(md5($key->getData()), 'd9d1f2cfc30034c1b12b5d9f3d03315b');
+    }
+
+    /**
+     * @expectedException Ivan1986\DebBundle\Exception\GpgNotFound
+     */
+    public function testGetNotExistKey()
+    {
+        $keys = $this->em->getRepository('Ivan1986DebBundle:GpgKey');
+        /** @var $keys GpgKeyRepository */
+        $id = 'ffffff';
+        $key = $keys->getFromServer($id, 'keyserver.ubuntu.com');
     }
 
 }
