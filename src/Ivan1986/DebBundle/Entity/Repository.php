83e835a24c28b99bf69510e3a9d31281d48742d5
Ivan Borzenkov <ivan1986@list.ru>
2012-04-20 10:11:10 +0400

    Поля для репозитория

--- src/Ivan1986/DebBundle/Entity/Repository.php
+++ src/Ivan1986/DebBundle/Entity/Repository.php
@@ -26,28 +26,50 @@ class Repository
     protected $id;
 
     /**
-     * @var string $url
-     *
-     * @ORM\Column(name="url", type="string", length=255)
-     */
-    protected $url;
-
-    /**
-     * @var GpgKey $key
-     *
-     * @ORM\OneToOne(targetEntity="GpgKey")
-     */
-    protected $key;
-
-    /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
         return $this->id;
     }
+
+    //<editor-fold defaultstate="collapsed" desc="Основные переменные, описывающие репозиторий">
+    /**
+     * @var string $url Адрес репозитория
+     *
+     * @ORM\Column(name="url", type="string")
+     */
+    protected $url;
+
+    /**
+     * @var string $name Релиз (stable,testing,sid, natty,oneiric)
+     *
+     * @ORM\Column(name="release", type="string")
+     */
+    protected $release;
+
+    /**
+     * @var string $components Компоненты
+     *
+     * @ORM\Column(name="components", type="array")
+     */
+    protected $components;
+
+    /**
+     * @var boolean $bin В репозитории есть бинарники
+     *
+     * @ORM\Column(name="bin", type="boolean")
+     */
+    protected $bin = true;
+
+    /**
+     * @var boolean $src В репозитории есть исходный код
+     *
+     * @ORM\Column(name="src", type="boolean")
+     */
+    protected $src = true;
 
     /**
      * Set url
@@ -64,10 +86,157 @@ class Repository
     /**
      * Get url
      *
-     * @return string 
+     * @return string
      */
     public function getUrl()
     {
         return $this->url;
+    }
+
+    /**
+     * Set release
+     *
+     * @param string $release
+     * @return Repository
+     */
+    public function setRelease($release)
+    {
+        $this->release = $release;
+        return $this;
+    }
+
+    /**
+     * Get release
+     *
+     * @return string
+     */
+    public function getRelease()
+    {
+        return $this->release;
+    }
+
+    /**
+     * Set components
+     *
+     * @param array $components
+     * @return Repository
+     */
+    public function setComponents($components)
+    {
+        $this->components = $components;
+        return $this;
+    }
+
+    /**
+     * Get components
+     *
+     * @return array
+     */
+    public function getComponents()
+    {
+        return $this->components;
+    }
+
+    /**
+     * Set bin
+     *
+     * @param boolean $bin
+     * @return Repository
+     */
+    public function setBin($bin)
+    {
+        $this->bin = $bin;
+        return $this;
+    }
+
+    /**
+     * Get bin
+     *
+     * @return boolean
+     */
+    public function getBin()
+    {
+        return $this->bin;
+    }
+
+    /**
+     * Set src
+     *
+     * @param boolean $src
+     * @return Repository
+     */
+    public function setSrc($src)
+    {
+        $this->src = $src;
+        return $this;
+    }
+
+    /**
+     * Get src
+     *
+     * @return boolean
+     */
+    public function getSrc()
+    {
+        return $this->src;
+    }
+    //</editor-fold>
+
+    /**
+     * @var string $name Имя репозитория
+     *
+     * @ORM\Column(name="name", type="string")
+     */
+    protected $name;
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Repository
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @var GpgKey $key Ключ
+     *
+     * @ORM\OneToOne(targetEntity="GpgKey")
+     */
+    protected $key;
+
+    /**
+     * Set key
+     *
+     * @param GpgKey $key
+     * @return Repository
+     */
+    public function setKey(GpgKey $key = null)
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    /**
+     * Get key
+     *
+     * @return GpgKey
+     */
+    public function getKey()
+    {
+        return $this->key;
     }
 }
