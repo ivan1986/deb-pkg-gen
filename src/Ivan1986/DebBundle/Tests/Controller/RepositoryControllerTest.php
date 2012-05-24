eb6078de77feca1e0d25093ae3e6aa84ef0c0079
Ivan Borzenkov <ivan1986@list.ru>
2012-05-24 12:39:58 +0400

    Тест

--- src/Ivan1986/DebBundle/Tests/Controller/RepositoryControllerTest.php
+++ src/Ivan1986/DebBundle/Tests/Controller/RepositoryControllerTest.php
@@ -6,49 +6,60 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RepositoryControllerTest extends WebTestCase
 {
-    /*
+    public $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('_submit')->form(array(
+            '_username'  => 'test',
+            '_password'  => 'test',
+        ));
+
+        $this->client->submit($form);
+    }
+
     public function testCompleteScenario()
     {
-        // Create a new client to browse the application
-        $client = static::createClient();
-
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/my_repos/');
-        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+        $crawler = $this->client->request('GET', '/profile/repos/');
+        $this->assertTrue(200 === $this->client->getResponse()->getStatusCode());
+        $crawler = $this->client->click($crawler->selectLink('Create a new entry')->link());
 
+        return;
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'repository[field_name]'  => 'Test',
+            'ivan1986_debbundle_repositorytype[field_name]'  => 'Test',
             // ... other fields to fill
         ));
 
-        $client->submit($form);
-        $crawler = $client->followRedirect();
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
 
         // Check data in the show view
         $this->assertTrue($crawler->filter('td:contains("Test")')->count() > 0);
 
         // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
+        $crawler = $this->client->click($crawler->selectLink('Edit')->link());
 
         $form = $crawler->selectButton('Edit')->form(array(
             'repository[field_name]'  => 'Foo',
             // ... other fields to fill
         ));
 
-        $client->submit($form);
-        $crawler = $client->followRedirect();
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
 
         // Check the element contains an attribute with value equals "Foo"
         $this->assertTrue($crawler->filter('[value="Foo"]')->count() > 0);
 
         // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
+        $this->client->submit($crawler->selectButton('Delete')->form());
+        $crawler = $this->client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->assertNotRegExp('/Foo/', $this->client->getResponse()->getContent());
     }
-    */
+
 }
