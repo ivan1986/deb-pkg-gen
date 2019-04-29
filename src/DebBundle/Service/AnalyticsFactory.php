3510dfa8aa8306c58d826335a7a275b49bc1549d
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2019-04-29 10:40:30 +0300

    update libs

--- src/DebBundle/Service/AnalyticsFactory.php
+++ src/DebBundle/Service/AnalyticsFactory.php
@@ -3,25 +3,14 @@
 namespace Ivan1986\DebBundle\Service;
 
 use FourLabs\GampBundle\Service\AnalyticsFactory as BaseAnalyticsFactory;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 class AnalyticsFactory extends BaseAnalyticsFactory
 {
-    public function createAnalytics(
-        RequestStack $requestStack,
-        $version,
-        $trackingId,
-        $ssl,
-        $anonymize,
-        $async,
-        $debug,
-        $sandbox)
-    {
-        $analytics = parent::createAnalytics($requestStack, $version, $trackingId, $ssl, $anonymize, $async, $debug, $sandbox);
+    public function createAnalytics() {
+        $analytics = parent::createAnalytics();
 
-        $request = $requestStack->getCurrentRequest();
-        if (null === $request || !$request->cookies->has('_ga')) {
-            $analytics->setClientId(mt_rand(1e10, 1e11 - 1).'.'.mt_rand(1e10, 1e11 - 1));
+        if (!$analytics->getClientId()) {
+            $analytics->setClientId(random_int(1e10, 1e11 - 1).'.'.random_int(1e10, 1e11 - 1));
         }
 
         return $analytics;
